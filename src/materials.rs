3b78efdb0e90ff28b6d6c3235dc8cfad85a25429
Matt Bray <mattjbray@gmail.com>
2020-12-28 18:38:56 +0000

    TIR and Shlick

--- src/materials.rs
+++ src/materials.rs
@@ -1,6 +1,7 @@
 use super::color::Color;
 use super::ray::{Hit, Material, Ray, Scatter};
 use super::vec3::Vec3;
+use rand::Rng;
 
 pub struct Lambertian {
     albedo: Color,
@@ -13,12 +14,7 @@ impl Lambertian {
 }
 
 impl Material for Lambertian {
-    fn scatter(
-        &self,
-        _r_in: &Ray,
-        hit: &Hit,
-        rng: &mut rand::rngs::ThreadRng,
-    ) -> Option<Scatter> {
+    fn scatter(&self, _r_in: &Ray, hit: &Hit, rng: &mut rand::rngs::ThreadRng) -> Option<Scatter> {
         let mut scatter_direction = hit.normal() + Vec3::gen_in_unit_sphere(rng);
         if scatter_direction.near_zero() {
             scatter_direction = hit.normal();
@@ -45,12 +41,7 @@ impl Metal {
 }
 
 impl Material for Metal {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        hit: &Hit,
-        rng: &mut rand::rngs::ThreadRng,
-    ) -> Option<Scatter> {
+    fn scatter(&self, r_in: &Ray, hit: &Hit, rng: &mut rand::rngs::ThreadRng) -> Option<Scatter> {
         let reflected = r_in.direction().unit().reflect(&hit.normal());
         let scattered = Ray::new(
             hit.position(),
@@ -75,15 +66,16 @@ impl Dielectric {
             index_of_refraction,
         }
     }
+
+    fn reflectance(&self, cosine: f64, ref_idx: f64) -> f64 {
+        let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+        let r0 = r0 * r0;
+        r0 + (1.0 - r0) * (1.0 - cosine).powf(5.)
+    }
 }
 
 impl Material for Dielectric {
-    fn scatter(
-        &self,
-        r_in: &Ray,
-        hit: &Hit,
-        _rng: &mut rand::rngs::ThreadRng,
-    ) -> Option<Scatter> {
+    fn scatter(&self, r_in: &Ray, hit: &Hit, rng: &mut rand::rngs::ThreadRng) -> Option<Scatter> {
         let attenuation = Color::new(1.0, 1.0, 1.0);
         let refraction_ratio = if hit.front_face() {
             1.0 / self.index_of_refraction
@@ -91,9 +83,20 @@ impl Material for Dielectric {
             self.index_of_refraction
         };
         let unit_direction = r_in.direction().unit();
-        let refracted = unit_direction.refract(&hit.normal(), refraction_ratio);
 
-        let scattered = Ray::new(hit.position(), refracted);
+        let cos_theta = f64::min(-unit_direction.dot(&hit.normal()), 1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract
+            || self.reflectance(cos_theta, refraction_ratio) > rng.gen_range((0.0)..1.0)
+        {
+            unit_direction.reflect(&hit.normal())
+        } else {
+            unit_direction.refract(&hit.normal(), refraction_ratio)
+        };
+
+        let scattered = Ray::new(hit.position(), direction);
 
         Some(Scatter::new(scattered, attenuation))
     }
